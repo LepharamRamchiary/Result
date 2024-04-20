4afc79aeb1e1ecc62bf1ba702cf0f31d1e86405d
Lepha <lepharamchiary@gmail.com>
2024-04-20 09:32:57 +0530

    first commit

---
+++ src/components/Hooks.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./HooksStyle.css"
+
+function Hooks() {
+    return (
+        <div className='result'>
+            <h1>HSLC Result Declared Today: 10:30 AM </h1>
+            <li><a href="https://sebaonline.org/">SEBA Official Site</a></li>
+
+            <li><a href="https://resultsassam.nic.in/">Result Assam Site</a></li>
+        </div>
+    )
+}
+
+export default Hooks
