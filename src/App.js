90c7f94f79ae3d0fecb556b910d5f70dab8ba21f
Lepha <lepharamchiary@gmail.com>
2024-04-24 10:56:29 +0530

    simple chnages

--- src/App.js
+++ src/App.js
@@ -2,14 +2,56 @@
 import './App.css';
 // import Hooks from './components/Hooks';
 // import Hook2 from './components/Hook2';
-import Hook from "./components/Hook4";
+import AddToDo from "./components/AddToDO";
+import ListTask from "./components/TaskList";
+import { useState } from 'react';
 
+let nextId = 3;
+const initialTodos = [
+  { id: 0, title: 'Buy milk', done: true },
+  { id: 1, title: 'Eat tacos', done: false },
+  { id: 2, title: 'Brew tea', done: false },
+];
 function App() {
+  const [todos, setTodos] = useState(initialTodos);
+
+  function handleAddTodo(title) {
+    setTodos([
+      ...todos,
+      {
+        id: nextId++,
+        title: title,
+        done: false
+      }
+    ]);
+  }
+
+  function handleChangeTodo(nextTodo) {
+    setTodos(todos.map(t => {
+      if (t.id === nextTodo.id) {
+        return nextTodo;
+      } else {
+        return t;
+      }
+    }));
+  }
+
+  function handleDeleteTodo(todoId) {
+    setTodos(
+      todos.filter(t => t.id !== todoId)
+    );
+  }
+
+
   return (
     <div className="App">
       {/* <Hook2 /> */}
       {/* <Hooks /> */}
-      <Hook />
+      <AddToDo onAddTodo={handleAddTodo} />
+      <ListTask todos={todos}
+      onChangeTodo={handleChangeTodo}
+      onDeleteTodo={handleDeleteTodo}
+      />
     </div>
   );
 }
