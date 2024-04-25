6ce6e6ad8e1b9b9576f463afdf61d4cc277a4858
Lepha <lepharamchiary@gmail.com>
2024-04-25 10:00:24 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,57 +1,51 @@
 // import logo from './logo.svg';
 import './App.css';
-// import Hooks from './components/Hooks';
+import Hooks from './components/Hook7';
 // import Hook2 from './components/Hook2';
-import AddToDo from "./components/AddToDO";
-import ListTask from "./components/TaskList";
-import { useState } from 'react';
+// import AddToDo from "./components/AddToDO";
+// import ListTask from "./components/TaskList";
+// import { useState } from 'react';
 
-let nextId = 3;
-const initialTodos = [
-  { id: 0, title: 'Buy milk', done: true },
-  { id: 1, title: 'Eat tacos', done: false },
-  { id: 2, title: 'Brew tea', done: false },
-];
+// let nextId = 3;
+// const initialTodos = [
+//   { id: 0, title: 'Buy milk', done: true },
+//   { id: 1, title: 'Eat tacos', done: false },
+//   { id: 2, title: 'Brew tea', done: false },
+// ];
 function App() {
-  const [todos, setTodos] = useState(initialTodos);
+//   const [todos, setTodos] = useState(initialTodos);
 
-  function handleAddTodo(title) {
-    setTodos([
-      ...todos,
-      {
-        id: nextId++,
-        title: title,
-        done: false
-      }
-    ]);
-  }
+//   function handleAddTodo(title) {
+//     setTodos([
+//       ...todos,
+//       {
+//         id: nextId++,
+//         title: title,
+//         done: false
+//       }
+//     ]);
+//   }
 
-  function handleChangeTodo(nextTodo) {
-    setTodos(todos.map(t => {
-      if (t.id === nextTodo.id) {
-        return nextTodo;
-      } else {
-        return t;
-      }
-    }));
-  }
+//   function handleChangeTodo(nextTodo) {
+//     setTodos(todos.map(t => {
+//       if (t.id === nextTodo.id) {
+//         return nextTodo;
+//       } else {
+//         return t;
+//       }
+//     }));
+//   }
 
-  function handleDeleteTodo(todoId) {
-    setTodos(
-      todos.filter(t => t.id !== todoId)
-    );
-  }
+//   function handleDeleteTodo(todoId) {
+//     setTodos(
+//       todos.filter(t => t.id !== todoId)
+//     );
+//   }
 
 
   return (
     <div className="App">
-      {/* <Hook2 /> */}
-      {/* <Hooks /> */}
-      <AddToDo onAddTodo={handleAddTodo} />
-      <ListTask todos={todos}
-      onChangeTodo={handleChangeTodo}
-      onDeleteTodo={handleDeleteTodo}
-      />
+      <Hooks />
     </div>
   );
 }
