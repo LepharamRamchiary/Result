07b84385095907eb9ecb4e254803f8e720c3d2f1
Lepha <lepharamchiary@gmail.com>
2024-04-25 10:00:32 +0530

    .

--- src/components/Hook6.js
+++ src/components/Hook6.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 
 function Hook6() {
+  const [count , setCount] = useState(0);
+
+  useEffect(() =>{
+    document.title = `You click ${count} times`;
+  })
+
   return (
     <div>
       <h1>Hello There</h1>
-      
+      <button onClick={() => setCount(count+1)} >click to add Num{count}</button>
     </div>
   )
 }
