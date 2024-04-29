05dbe98f890f93f233c850441d9c15052739dc86
Lepha <lepharamchiary@gmail.com>
2024-04-29 09:20:22 +0530

    didMount

---
+++ src/components/Hook8.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { useEffect } from 'react';
+import { useState } from 'react'
+
+function Hook8() {
+    const [x, setX] = useState(0);
+    const [y, setY] = useState(0);
+
+    const logMousePosition = e => {
+        console.log('Mouse event');
+        setX(e.clientX)
+        setY(e.clientY);
+    }
+
+    useEffect(() => {
+        console.log("useEffect called");
+        window.addEventListener('mousemove', logMousePosition);
+
+        return () => {
+            console.log('code');
+            window.removeEventListener('mousemove', logMousePosition);
+        }
+    }, [])
+  return (
+    <div>Hooks X-{x} Y-{y}</div>
+  )
+}
+
+export default Hook8
